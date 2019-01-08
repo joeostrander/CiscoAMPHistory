9bb01c2231f63d055ca8e84da6f1d830619dcea7
joeostrander <ostrander.jm@gmail.com>
2019-01-08 09:56:07 -0600

    Add project files.

---
+++ CiscoAMPHistory/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CiscoAMPHistory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonLoadData = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "History DB file:";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(12, 25);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(482, 20);
+            this.textBoxFilePath.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(500, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonLoadData
+            // 
+            this.buttonLoadData.Location = new System.Drawing.Point(612, 23);
+            this.buttonLoadData.Name = "buttonLoadData";
+            this.buttonLoadData.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadData.TabIndex = 3;
+            this.buttonLoadData.Text = "&Load Data";
+            this.buttonLoadData.UseVisualStyleBackColor = true;
+            this.buttonLoadData.Click += new System.EventHandler(this.buttonLoadData_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.OldLace;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(675, 317);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 381);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonLoadData);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(715, 420);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonLoadData;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CiscoAMPHistory/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Collections.Specialized;
+
+namespace CiscoAMPHistory
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        public delegate void AddDataGridViewRowCallback(DataGridView dgv, DataGridViewRow row);
+        
+
+        private string dbPath;
+
+        private string strParentDir = Path.GetDirectoryName(Application.ExecutablePath) + "\\";
+       
+        NameValueCollection listActionType = new NameValueCollection()
+        {
+            { "1", "Create" },
+            { "2", "Execute" },
+            { "4", "Scan" },
+            { "6", "Quarantine" },
+            { "7", "Quarantine" },
+            { "22", "Move" },
+            { "40", "Open" }
+        };
+
+             
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Application.ProductName;
+            
+            dataGridView1.Columns.Add("columnCount", "#");
+            dataGridView1.Columns.Add("columnPath", "Path");
+            dataGridView1.Columns.Add("columnLastRef", "LastRef");
+            dataGridView1.Columns.Add("columnDescription", "Description");
+            dataGridView1.Columns.Add("columnHash", "Hash");
+
+            dataGridView1.Columns["columnCount"].Width = 40;
+            dataGridView1.Columns["columnPath"].Width = 350;
+            dataGridView1.Columns["columnLastRef"].Width = 140;
+            dataGridView1.Columns["columnDescription"].Width = 100;
+            dataGridView1.Columns["columnHash"].Width = 200;
+
+
+
+            AddDLL();
+            
+        }
+
+        private void AddDLL()
+        {
+            string strCurrentPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process);
+            System.Environment.SetEnvironmentVariable("PATH", strCurrentPath + ";" + strParentDir, EnvironmentVariableTarget.Process);
+            Directory.SetCurrentDirectory(strParentDir);
+
+            string strFile1 = "System.Data.SQLite.dll";
+            string strFile2 = "SQLite.Interop.dll";
+            System.Reflection.AssemblyName file1;
+            
+            try
+            {
+                file1 = System.Reflection.AssemblyName.GetAssemblyName(strFile1);
+                
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    try
+                    {
+                        //Create DLL from resources
+                        if (!File.Exists(strParentDir + strFile2))
+                        {
+                            MemoryStream ms1 = new MemoryStream(Properties.Resources.System_Data_SQLite);
+                            FileStream fs1 = File.OpenWrite(strParentDir + strFile1);
+                            ms1.WriteTo(fs1);
+                        }
+
+                        if (!File.Exists(strParentDir + strFile2))
+                        {
+                            MemoryStream ms2 = new MemoryStream(Properties.Resources.SQLite_Interop);
+                            FileStream fs2 = File.OpenWrite(strParentDir + strFile2);
+                            ms2.WriteTo(fs2);
+                        }
+
+                        file1 = System.Reflection.AssemblyName.GetAssemblyName(strFile1);
+                    }
+                    catch (Exception ex3)
+                    {
+                        //Do nothing?
+                    }
+                    
+                }
+                catch (Exception ex2)
+                {
+                    MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    Application.Exit();
+                }
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = strParentDir;
+            openFileDialog1.Title = "Select the Cisco AMP history file...";
+            openFileDialog1.Filter = "historyex.db file | *.db";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void buttonLoadData_Click(object sender, EventArgs e)
+        {
+            LoadDataAsync();
+        }
+
+        private bool checkInputFile()
+        {
+            dbPath = textBoxFilePath.Text;
+
+            if (!File.Exists(dbPath))
+            {
+                MessageBox.Show("Please select a valid historyex.db file!", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+            return true;
+        }
+
+        private void enableControls(bool enable)
+        {
+            textBoxFilePath.Enabled = enable;
+            button1.Enabled = enable;
+            buttonLoadData.Enabled = enable;
+            dataGridView1.Enabled = enable;
+        }
+
+
+        private async Task LoadDataAsync()
+        {
+            
+            dataGridView1.Rows.Clear();
+            enableControls(false);
+
+            if (checkInputFile())
+            {
+                await Task.Run(() => LoadData());
+            }
+
+            //ColorLines(dataGridView1);
+            enableControls(true);
+        }
+
+        private async void LoadData() { 
+            
+
+        //listActionType["7"]);   
+        
+            string connString = string.Format("Data Source={0}", dbPath);
+
+            var sql = "SELECT path, hash, lastref, type FROM path_history ORDER by lastref desc";
+
+            using (SQLiteConnection con = new SQLiteConnection())
+            {
+                con.ConnectionString = connString;
+
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, con))
+                {
+                    con.Open();
+
+                    int count = 0;
+
+                    DataSet ds = new DataSet();
+                    ds.Tables.Add("history");
+                    ds.Tables["history"].Columns.Add("#");
+                    ds.Tables["history"].Columns.Add("Path");
+                    ds.Tables["history"].Columns.Add("LastRef");
+                    ds.Tables["history"].Columns.Add("Description");
+                    ds.Tables["history"].Columns.Add("Hash");
+                    
+
+                    using (var dr = await cmd.ExecuteReaderAsync()) {
+                        
+                        while (await dr.ReadAsync())
+                        {
+
+                            string strHash = dr.GetString(1);
+                            long secs = (long)dr.GetValue(2);
+                            DateTime dtLastRef = DateTimeOffset.FromUnixTimeSeconds(secs).LocalDateTime;
+
+                            string desc = listActionType[dr.GetValue(3).ToString()];
+                            
+                            count++;
+                            DataGridViewRow row = new DataGridViewRow();
+                            row.CreateCells(dataGridView1);
+                            row.Cells[0].Value = count;
+                            row.Cells[1].Value = "";
+                            row.Cells[2].Value = dtLastRef;
+                            row.Cells[3].Value = desc;
+                            row.Cells[4].Value = strHash;
+                            
+
+
+                            
+                            var path_numeric = dr.GetString(0);
+                            string[] arrPath = path_numeric.Split('\\');
+
+                            for (int i = 0; i < arrPath.Length; i++)
+                            {
+                                // foreach (var path in arrPath)
+                                var path = arrPath[i];
+                                var sqlTmp = string.Format("select name from component where id={0} LIMIT 1", path);
+
+                                using (SQLiteCommand cmdTmp = new SQLiteCommand(sqlTmp, con))
+                                {
+                                    using (var drTmp = await cmdTmp.ExecuteReaderAsync())
+                                    {
+                                        while (await drTmp.ReadAsync())
+                                        {
+                                            arrPath[i] = drTmp.GetString(0);
+                                            //Console.WriteLine("??? {0}", drTmp.GetValue(0).ToString());
+                                        }
+                                    }
+                                }
+
+                            }
+
+                            string fullPath = string.Join("\\", arrPath);
+                            row.Cells[1].Value = fullPath;
+                            
+
+                            if (dataGridView1.InvokeRequired)
+                            {
+                                dataGridView1.Invoke(new AddDataGridViewRowCallback(AddToDataGridView), new object[] { dataGridView1, row });
+                            }
+                            else
+                            {
+                                AddToDataGridView(dataGridView1, row);
+
+                            }
+                        }
+                    }
+
+                    
+
+                    
+                }
+
+                
+                
+            }
+            
+            
+
+        }
+
+        private void AddToDataGridView(DataGridView dgv, DataGridViewRow row)
+        {
+            dgv.Rows.Add(row);
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+            {
+                textBoxFilePath.Text = files[0];
+
+                LoadDataAsync();
+
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        /*
+
+        private void ColorLines(DataGridView dgv)
+        {
+            int count = 0;
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                count += 1;
+                
+                if (count % 2 == 0)
+                {
+                    row.DefaultCellStyle.BackColor = Color.OldLace;
+                }
+                else
+                {
+                    row.DefaultCellStyle.BackColor = Color.White;
+                }
+
+                row.DefaultCellStyle.ForeColor = Color.Black;
+
+            }
+        }
+        */
+    }
+
+
+}
